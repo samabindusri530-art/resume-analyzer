55b25d1072d556eabcca0c042df524dabf2f285c
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 11:53:27 +0530

    Create skills.py

---
+++ skills.py
@@ -0,0 +1,24 @@
+job_roles = {
+    "Software Engineer": [
+        "python",
+        "sql",
+        "git",
+        "data structures",
+        "oops"
+    ],
+
+    "Frontend Developer": [
+        "html",
+        "css",
+        "javascript",
+        "react"
+    ],
+
+    "Data Scientist": [
+        "python",
+        "machine learning",
+        "pandas",
+        "numpy",
+        "sql"
+    ]
+}
