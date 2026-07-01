6305a60a7b4ebe03aa3631355cc8fc583ae24032
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 11:54:01 +0530

    Create analyzer.py

---
+++ analyzer.py
@@ -0,0 +1,38 @@
+import PyPDF2
+from skills import job_roles
+
+
+def extract_text(pdf_file):
+
+    reader = PyPDF2.PdfReader(pdf_file)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
+
+    return text.lower()
+
+
+
+def analyze_resume(text, role):
+
+    required_skills = job_roles[role]
+
+    found = []
+    missing = []
+
+    for skill in required_skills:
+
+        if skill in text:
+            found.append(skill)
+
+        else:
+            missing.append(skill)
+
+    score = int((len(found) / len(required_skills)) * 100)
+
+    return found, missing, score
