df3fc6819432fb94bac0a89370cfd0eba6c27859
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 15:38:53 +0530

    Update nav.py

--- nav.py
+++ nav.py
@@ -12,32 +12,10 @@ def show_nav():
             "Login",
             "Resume Analyzer",
             "Resume Tips",
-            "Career Advisor",
+            "AI Career Advisor",
             "Job Roles",
-            "About"
+            "About Project"
         ]
     )
 
-    if page == "Home":
-        st.rerun()   # ✅ FIX HERE
-
-    elif page == "Register":
-        st.switch_page("1_Register")
-
-    elif page == "Login":
-        st.switch_page("2_Login")
-
-    elif page == "Resume Analyzer":
-        st.switch_page("3_Resume_Analyzer")
-
-    elif page == "Resume Tips":
-        st.switch_page("4_Resume_Tips")
-
-    elif page == "Career Advisor":
-        st.switch_page("5_AI_Career_Advisor")
-
-    elif page == "Job Roles":
-        st.switch_page("6_Job_Roles")
-
-    elif page == "About":
-        st.switch_page("7_About_Project")
+    st.session_state["nav_page"] = page
