604750405a265734625971f7f1dad1584c5374ed
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 13:33:48 +0530

    app.py

--- app.py
+++ app.py
@@ -8,14 +8,20 @@ st.write("Welcome to AI Resume Analyzer")
 
 st.subheader("Choose a Page")
 
+if st.button("Register"):
+    st.switch_page("pages/1_Register.py")
+
+if st.button("Login"):
+    st.switch_page("pages/2_Login.py")
+
 if st.button("Resume Analyzer"):
-    st.switch_page("pages/1_Resume_Analyzer.py")
+    st.switch_page("pages/3_Resume_Analyzer.py")
 
 if st.button("Resume Tips"):
-    st.switch_page("pages/2_Resume_Tips.py")
+    st.switch_page("pages/4_Resume_Tips.py")
 
 if st.button("Job Roles"):
-    st.switch_page("pages/3_Job_Roles.py")
+    st.switch_page("pages/5_Job_Roles.py")
 
 if st.button("About Project"):
-    st.switch_page("pages/4_About_Project.py")
+    st.switch_page("pages/6_About_Project.py")
