12651654a26738878c40e54a75b562696a0f3b7c
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 12:25:28 +0530

    page/2_Resume_Tips.py

---
+++ pages/2_Resume_Tips.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Resume Improvement Tips")
+
+tips = [
+
+    "Add Projects Section",
+
+    "Mention Technical Skills",
+
+    "Add Certifications",
+
+    "Keep Resume One Page",
+
+    "Add GitHub Profile",
+
+    "Use Professional Email",
+
+    "Mention Achievements"
+
+]
+
+for tip in tips:
+    st.write("•", tip)
