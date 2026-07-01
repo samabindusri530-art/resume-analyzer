2ee7f1503079d9bbd1ac2f4f347d691f60b5889b
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 15:22:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
+from nav import show_nav
 
-st.set_page_config(page_title="AI Resume Analyzer", layout="centered")
+st.set_page_config(page_title="AI Resume Analyzer")
+
+show_nav()
 
 st.title("🤖 AI Resume Analyzer")
 st.write("Welcome to AI Resume Analyzer")
 
 st.subheader("Choose a Page")
-
-# ---------------- NAVIGATION BUTTONS ----------------
 
 if st.button("📝 Register"):
     st.switch_page("1_Register")
@@ -21,11 +22,11 @@ if st.button("📄 Resume Analyzer"):
 if st.button("🤖 Resume Tips"):
     st.switch_page("4_Resume_Tips")
 
-if st.button("🎯 AI Career Advisor"):
+if st.button("🎯 Career Advisor"):
     st.switch_page("5_AI_Career_Advisor")
 
 if st.button("💼 Job Roles"):
     st.switch_page("6_Job_Roles")
 
-if st.button("ℹ About Project"):
+if st.button("ℹ About"):
     st.switch_page("7_About_Project")
