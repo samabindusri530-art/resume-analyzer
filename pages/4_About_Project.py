280d161bd37c51e57d760405b1ad5a5117a5f2f7
samabindusri530-art <samabindusri530@gmail.com>
2026-07-01 12:32:37 +0530

    page/4_About_Project.py

---
+++ pages/4_About_Project.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("About Project")
+
+st.write(
+    "AI Resume Analyzer is a project "
+    "that analyzes resume PDF files."
+)
+
+st.write(
+    "It compares user skills with "
+    "job role requirements."
+)
+
+st.write(
+    "Built using Python, Streamlit and PyPDF2"
+)
